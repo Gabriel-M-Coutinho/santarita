aa051f124d0752726db25e2351e287a103a7a49f
BulboGC <gabrielshot1@gmail.com>
2023-06-02 13:10:32 -0300

    criado meta para verificação

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
+      <meta name="google-site-verification" content='googlea77bf85c64bb5634.html'></meta>
       <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
